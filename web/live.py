eaf63363d7a0ea7ba955c68a6a4149e854f8e18c
Jeffrey Wang <jeffrey@amplitude.com>
2018-03-24 11:36:26 -0700

    Increase online time to 10m.

--- web/live.py
+++ web/live.py
@@ -13,8 +13,8 @@ live = Blueprint('live', __name__)
 def live_index():
     active_games = db_service.get_all_active_games()
 
-    two_minutes_ago = datetime.datetime.utcnow() - datetime.timedelta(minutes=2)
-    online_users = db_service.get_users_online_since(two_minutes_ago)
+    ten_minutes_ago = datetime.datetime.utcnow() - datetime.timedelta(minutes=10)
+    online_users = db_service.get_users_online_since(ten_minutes_ago)
 
     return json.dumps({
         'games': [
