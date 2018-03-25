d9165441c294c262e55d2e698c625499a9ad19e4
Jeffrey Wang <jeffrey@amplitude.com>
2018-03-25 00:26:55 -0700

    Enable compression of webpack bundle.

--- ui/webpack/webpack.prod.config.js
+++ ui/webpack/webpack.prod.config.js
@@ -2,14 +2,17 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.config.js');
 
+const CompressionPlugin = require('compression-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
     plugins: [
-        new UglifyJsPlugin(),
         new webpack.DefinePlugin({
             AMPLITUDE_API_KEY: JSON.stringify('8f1fcc6159b9141780ea8f019a988366'),
+            'process.env.NODE_ENV': JSON.stringify('production'),
         }),
+        new UglifyJsPlugin(),
+        new CompressionPlugin(),
     ],
 });
